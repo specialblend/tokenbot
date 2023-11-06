5d2657a136fc87aaab9d8a7cc44892033a9ba4ed
specialblend <cj@specialblend.dev>
2023-11-06 16:28:00 -0600

    chore: clean up loader

--- web/app/components/Placeholder.tsx
+++ web/app/components/Placeholder.tsx
@@ -32,12 +32,6 @@ export function PlaceholderError() {
       <div className="flex justify-center pt-36">
         <div className="text-center p-4">
           <div className={`p-2`}>⚠️ There was an issue loading the data.</div>
-          <div className="ink-dimmed p-2">
-            <p>
-              Be first to send thanks to other players by mentioning them in
-              Slack with <span className="marker-dimmed ink-base">@thanks</span>
-            </p>
-          </div>
           <div className="p-2 text-lg">
             <Link href="/rules">⁉️ See rules</Link>
           </div>
@@ -56,10 +50,7 @@ export function PlaceholderLoading() {
             <Spinner />
           </div>
           <div className="ink-dimmed p-2">
-            <p>
-              Be first to send thanks to other players by mentioning them in
-              Slack with <span className="marker-dimmed ink-base">@thanks</span>
-            </p>
+            <p>Loading player data...</p>
           </div>
           <div className="p-2 text-lg">
             <Link href="/rules">⁉️ See rules</Link>
