1550a4ffff1b81088e6982ff529d8b73e57305aa
specialblend <cj@specialblend.dev>
2023-11-07 12:30:00 -0600

    feat: collect default metrics

--- web/server.js
+++ web/server.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const next = require('next');
+const {
+    collectDefaultMetrics,
+    Registry,
+    Gauge,
+} = require('prom-client');
 
 const hostname = "localhost";
 const port = parseInt(process.env.PORT, 10) || 3000;
@@ -10,10 +15,32 @@ const nextApp = next({
     port,
 });
 
-nextApp.prepare().then(() => {
-    const appServer = express();
-    appServer.all('*', nextApp.getRequestHandler());
-    appServer.listen(port, () => {
-        console.log(`app ready at http://localhost:${port}`);
+function App() {
+    const app = express();
+    app.all('*', nextApp.getRequestHandler());
+    return app;
+}
+
+function Metrics() {
+    const app = express();
+    const register = new Registry();
+
+    collectDefaultMetrics({ register });
+
+    // const players = new Gauge();
+
+    app.get("/metrics", async (req, res) => {
+        res.set("Content-Type", register.contentType);
+        res.end(await register.metrics())
     });
-}).catch(console.error);
+
+    return app;
+}
+
+nextApp
+    .prepare()
+    .then(() => App().listen(port))
+    .then(() => console.log(`app ready at http://${hostname}:${port}`))
+    .then(() => Metrics().listen(9090))
+    .then(() => console.log(`metrics ready at http://${hostname}:${9090}`))
+    .catch(console.error);
